dd911d0dfa03bbecfaf1854312d1451fe20f376b
Abdoullah Bougataya <sience.story@gmail.com>
2023-03-04 18:47:45 +0100

    Initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
-import './globals.css'
+import '../styles/globals.css';
+import Providers from './providers';
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,8 +12,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+    <Providers>
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    </Providers>
   )
 }
